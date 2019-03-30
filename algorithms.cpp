c3defbff5105862a9d9a1e1c86e330e9e0b0fcc9
Unknown <bea.smendes98@gmail.com>
2019-03-30 23:21:33 +0000

    started iterative dfs

--- algorithms.cpp
+++ algorithms.cpp
@@ -330,9 +330,94 @@ Node alg_greedy(Node startN, Map currMap)
 Node alg_progDeep(Node startN, Map currMap)
 {
     Node ret;
+    int tempMaxDepth=1;
+
+    map<vector<pair<int, int>>, int> prevStates;
+    pair<map<vector<pair<int, int>>, int>::iterator, bool> insertRet;
+
+    // Using lambda to compare elements.
+    auto cmp = [](Node left, Node right) {
+        return (left.depth) < (right.depth);
+    };
+
+    priority_queue<Node, vector<Node>, decltype(cmp)> p_queue(cmp);
+
+
+    p_queue.push(startN);
+    prevStates.insert(pair<vector<pair<int, int>>, int>(robotToPositions(startN.robots), startN.depth));
+
+    int n_expansions = 0;
+
+    while (p_queue.size() > 0)
+    {
+        Node first = p_queue.top();
+        p_queue.pop();
+
+        //Para evitar percorrer infinitamente
+        if (first.depth > MAX_DEPTH)
+            continue;
+        else if (first.depth > tempMaxDepth){
+            tempMaxDepth++;
+            while(!p_queue.empty()){
+                p_queue.pop();
+            }
+        }
+
+        //TODO: Apenas no DFS, os robots e direções passar a random para mitigar ciclos & remover max_depth depois(?)
+
+        //itera pelos robots
+        for (u_int r = 0; r < currMap.robots.size(); r++)
+        {
+            //itera pelas direções
+            for (u_int d = 0; d < 4; d++)
+            {
+                currMap.robots = first.robots;
+                currMap.createLayoutWithRobots();
+                //Only expand useful moves
+                if (currMap.moveRobot(r, d) != -1)
+                {
+                    n_expansions++;
+                    Node toInsert;
+                    toInsert.robots = currMap.robots;
+                    toInsert.depth = first.depth + 1;
+                    toInsert.moveSeq = first.moveSeq;
+                    toInsert.moveSeq.push_back(make_pair(r, d));
+
+                    //Check if gameover
+                    if (currMap.checkGameOver())
+                    {
+                        toInsert.expansions = n_expansions;
+                        return toInsert;
+                    }
+
+                        //Push to queue
+                    else
+                    {
+                        insertRet = prevStates.insert(pair<vector<pair<int, int>>, int>(robotToPositions(toInsert.robots), toInsert.depth));
+                        if (!insertRet.second) //Já foi percorrido
+                        {
+                            if (toInsert.depth < insertRet.first->second) //Chegou com profundidade menor portanto vai para a p_queue
+                            {
+                                insertRet.first->second = toInsert.depth;
+                                p_queue.push(toInsert);
+                            }
+                            else // Ignorar node porque já passou lá com profundidade menor
+                            {
+                                continue;
+                            }
+                        }
+                        else //Ainda não vou percorrido
+                            p_queue.push(toInsert);
+                    }
+                }
+            }
+        }
+    }
 
     //Se chegar aqui, quer dizer que ficou sem nós por expandir, logo não há solução
+
     ret.depth = -1; // Depth = -1 quer dizer que não encontrou solução
+
     return ret;
 }
 
